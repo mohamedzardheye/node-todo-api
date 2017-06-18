752ddc9dc4dee50178befda1c9cea22d284e7f3f
mohamed mohamud zardheye <mohamedzardheye@gmail.com>
2017-06-18 17:23:57 +0300

    deploy to heroku

--- server/server.js
+++ server/server.js
@@ -2,30 +2,23 @@ var express = require('express');
 var bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
-
-
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {users} = require('./models/users');
 
-
-
 var app = express();
+const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
-
 app.post('/todos', (req, res) => {
   var todo = new Todo ({
     text: req.body.text
   });
-
   var otherTodo = new Todo({
     text:req.body.text,
     completed: true
   });
-
   otherTodo.save().then((doc) => {
     res.send(doc);
-
   }, (e) => {
     res.status(400).send(e);
   });
@@ -59,8 +52,8 @@ app.get('/todos/:id', (req,res) => {
 });
 
 
-app.listen(3000, () => {
-  console.log('Started on port 3000');
+app.listen(port, () => {
+  console.log('Started up at port'+port);
 });
 
 module.exports = {app};
